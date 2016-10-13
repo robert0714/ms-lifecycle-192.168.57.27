5953cd5f937a147a33c23e81398b8887081a4fe9
Robert <robert0714@gmail.com>
2016-10-13 01:49:43 -0500

    Update bootstrap4CentOs_ansible.sh

--- scripts/bootstrap4CentOs_ansible.sh
+++ scripts/bootstrap4CentOs_ansible.sh
@@ -2,55 +2,17 @@
 
 set -e
 
-echo "Installing Ansible  on CentOS..."
-#apt-get install -y software-properties-common
-#apt-add-repository ppa:ansible/ansible
-#apt-get update
-#apt-get install -y --force-yes ansible
-#cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
+echo "Installing Ansible  on CentOS..." 
 
-#wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
-#rpm -ivh epel-release-7-8.noarch.rpm
-#rm -rf epel-release-7-8.noarch.rpm
-#su -c 'rpm -Uvh http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm'  /
+ yum install -y git
+ git clone git://github.com/ansible/ansible.git --recursive
+ cd ./ansible
+ make rpm
+ sudo rpm -Uvh ./rpm-build/ansible-*.noarch.rpm
 
-sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/7/
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF
-
-yum -y install ansible
-yum -y install jq
-#yum -y update
-
-#sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
-#[dockerrepo]
-#name=Docker Repository
-#baseurl=https://yum.dockerproject.org/repo/main/centos/7/
-#enabled=1
-#gpgcheck=1
-#gpgkey=https://yum.dockerproject.org/gpg
-#EOF
-
-#sudo yum  -y install docker-engine
-#sudo chkconfig docker on
-#sudo service docker start
+yum  install -y ansible
+yum  install -y jq
+ 
 
 cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
-
-
-
-
-
-#apt-get update -y
-#apt-get install -y python-pip python-dev
-#pip install ansible==1.9.2
-#mkdir -p /etc/ansible
-#touch /etc/ansible/hosts
-#cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
-#mkdir -p /etc/ansible/callback_plugins/
-#cp /vagrant/ansible/plugins/human_log.py /etc/ansible/callback_plugins/human_log.py
+ 
