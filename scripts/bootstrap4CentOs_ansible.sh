a688f36cdf176db3f9ef16fd89cd57290c91afee
Robert <robert0714@gmail.com>
2016-10-26 05:20:21 -0500

    Update bootstrap4CentOs_ansible.sh

--- scripts/bootstrap4CentOs_ansible.sh
+++ scripts/bootstrap4CentOs_ansible.sh
@@ -3,7 +3,21 @@
 set -e
 
 echo "Installing Ansible  on CentOS..." 
+A=`yum info ansible`
+testing=`[[ $A =~ 'Ansible' ]]`
+echo $testing
+echo  "############# ${testing} ##############"
 
+if [ $testing != ""  ]; then
+     echo "Ansible is already installed."
+else
+      echo "Ansible is not installed."
+      yum install -y wget
+      wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
+      rpm -ivh  epel-release-7-8.noarch.rpm
+      rm -rf epel-release-7-8.noarch.rpm
+      
+fi
  yum install -y git
  git clone git://github.com/ansible/ansible.git --recursive
  cd ./ansible
