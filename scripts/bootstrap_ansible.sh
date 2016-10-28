d8d5deaf69637f39224376930c57cb3be7431aca
Robert <robert0714@gmail.com>
2016-10-27 20:31:50 -0500

    Update bootstrap_ansible.sh

--- scripts/bootstrap_ansible.sh
+++ scripts/bootstrap_ansible.sh
@@ -18,11 +18,6 @@ cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
 echo "Installing Git..."
 apt-get install -y git
 
-#apt-get update -y
-#apt-get install -y python-pip python-dev
-#pip install ansible==1.9.2
-#mkdir -p /etc/ansible
-#touch /etc/ansible/hosts
-#cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
-#mkdir -p /etc/ansible/callback_plugins/
-#cp /vagrant/ansible/plugins/human_log.py /etc/ansible/callback_plugins/human_log.py
+
+echo "Script  Endbootstrap_ansible.sh..."
+ 
