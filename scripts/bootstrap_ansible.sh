08f7e43e562dbb862d6ba0879d1326a179df494b
Robert <robert0714@gmail.com>
2016-10-27 20:46:06 -0500

    Update bootstrap_ansible.sh

--- scripts/bootstrap_ansible.sh
+++ scripts/bootstrap_ansible.sh
@@ -2,22 +2,21 @@
 
 set -e
 
-echo "Updating Ubuntu ..."
-apt-get update -y
-
 echo "Installing Repository..."
 apt-get install -y software-properties-common
 apt-add-repository ppa:ansible/ansible
+
+echo "Installing Git..."
+apt-get install -y git
+
+echo "Updating Ubuntu ..."
+apt-get update -y
 
 echo "Installing Ansible..."
 apt-get install -y   ansible
 
 echo "Copying Ansible Configuration..."
 cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
-
-echo "Installing Git..."
-apt-get install -y git
-
 
 echo "Script  Endbootstrap_ansible.sh..."
  
